b8ee18781c1fd4041e2fb165de642e8386db9781
Dave Dribin <dave-vcs@dribin.org>
2021-09-12 00:31:27 -0500

    Add basic SDL sound shell the outputs a sine wave

--- rtl/fpgapu_sdl_main.cpp
+++ rtl/fpgapu_sdl_main.cpp
@@ -1,6 +1,23 @@
+#include <math.h>
 #include <SDL.h>
 #include <verilated.h>
 #include "Vfpgapu_sdl_top.h"
+
+float audio_time = 0;
+float freq = 440;
+
+void callback(void* userdata, Uint8* stream, int len) {
+    short * snd = reinterpret_cast<short*>(stream);
+    len /= sizeof(*snd);
+    for(int i = 0; i < len; i++) //Fill array with frequencies, mathy-math stuff
+    {
+        snd[i] = 32000 * sin(audio_time);
+        
+        audio_time += freq * M_PI*2 / 48000.0;
+        if(audio_time >= M_PI*2)
+            audio_time -= M_PI*2;
+    }
+}
 
 int main(int argc, char* argv[])
 {
@@ -25,6 +42,29 @@ int main(int argc, char* argv[])
         SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Renderer creation failed: %s\n", SDL_GetError());
         return 1;
     }
+
+    SDL_Init(SDL_INIT_AUDIO);
+    SDL_AudioSpec spec, aspec; // the specs of our piece of "music"
+    SDL_zero(spec);
+    spec.freq = 48000; //declare specs
+    spec.format = AUDIO_S16SYS;
+    spec.channels = 1;
+    spec.samples = 4096;
+    spec.callback = callback;
+    spec.userdata = NULL;
+
+      //Open audio, if error, print
+    int id;
+    if ((id = SDL_OpenAudioDevice(nullptr, 0, &spec, &aspec, SDL_AUDIO_ALLOW_ANY_CHANGE)) <= 0 )
+    {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't open audio: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    freq = 440;
+
+    /* Start playing, "unpause" */
+    SDL_PauseAudioDevice(id, 0);
 
     bool running = true;
     while (running) {
--- rtl/fpgapu_sdl_top.v
+++ rtl/fpgapu_sdl_top.v
@@ -5,6 +5,46 @@ module fpga_sdl_top (
   output wire [7:0] o_audio_sample
 );
 
-assign o_audio_sample = 8'd0;
+  wire w_tick_stb;
+  wire w_beat_stb;
+  timing_strobe_generator pulse_generator (
+    .i_clk(i_clk),
+    .o_tick_stb(w_tick_stb),
+    .o_beat_stb(w_beat_stb)
+  );
   
+  wire [8:0] w_compare_pulse_1;
+  wire w_frame_pulse;
+  channel_1_pulse pulse_1(
+    .i_clk(i_clk),
+    .i_tick_stb(w_tick_stb),
+    .i_note_stb(w_beat_stb),
+    .o_output(w_compare_pulse_1),
+    .o_frame_pulse(w_frame_pulse)
+  );
+
+  wire [8:0] w_compare_pulse_2;
+  channel_2_pulse pulse_2(
+    .i_clk(i_clk),
+    .i_tick_stb(w_tick_stb),
+    .i_note_stb(w_beat_stb),
+    .o_output(w_compare_pulse_2),
+    .o_frame_pulse()
+  );
+
+  wire [8:0] w_triangle_3_output;
+  channel_3_triangle triangle_3(
+    .i_clk(i_clk),
+    .i_tick_stb(w_tick_stb),
+    .i_note_stb(w_beat_stb),
+    .o_output(w_triangle_3_output),
+    .o_frame_pulse()
+  );
+
+  // Mixer
+  wire [8:0] w_compare =
+    (w_compare_pulse_1 + w_compare_pulse_2 + w_triangle_3_output);
+
+  assign o_audio_sample = w_compare[7:0];
+
 endmodule
