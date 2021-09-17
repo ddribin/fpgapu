f19858de5803188522ccc96f1750c18a3fe2576d
Dave Dribin <dave-vcs@dribin.org>
2021-09-16 22:27:33 -0500

    Clean up pwm_top.v
    Use audio_processing_unit. Extract all switch synchronizers to go_board_switches.v

---
+++ rtl/go_board_switches.v
@@ -0,0 +1,39 @@
+`default_nettype none
+
+module go_board_switches (
+  input wire        i_clk,
+  input wire  [3:0] i_switches,
+  output wire [3:0] o_switches
+);
+  
+  wire w_switch_1;
+  synchronizer switch_1_sync (
+    .i_clk(i_clk),
+    .i_input(i_switches[0]),
+    .o_input_sync(w_switch_1)
+  );
+
+  wire w_switch_2;
+  synchronizer switch_2_sync (
+    .i_clk(i_clk),
+    .i_input(i_switches[1]),
+    .o_input_sync(w_switch_2)
+  );
+
+  wire w_switch_3;
+  synchronizer switch_3_sync (
+    .i_clk(i_clk),
+    .i_input(i_switches[2]),
+    .o_input_sync(w_switch_3)
+  );
+  
+  wire w_switch_4;
+  synchronizer switch_4_sync (
+    .i_clk(i_clk),
+    .i_input(i_switches[3]),
+    .o_input_sync(w_switch_4)
+  );
+
+  assign o_switches = {w_switch_4, w_switch_3, w_switch_2, w_switch_1};
+
+endmodule
--- rtl/pwm_top.v
+++ rtl/pwm_top.v
@@ -6,98 +6,34 @@ module pwm_top (
   input wire  i_Switch_1,
   input wire  i_Switch_2,
   input wire  i_Switch_3,
+  input wire  i_Switch_4,
 
   output wire io_PMOD_1,
   output wire io_PMOD_2,
-  output wire io_PMOD_3,
-  output wire o_LED_1
+  output wire io_PMOD_3
 );
   wire i_clk = i_Clk;
-  
-  wire w_Switch_1;
-  synchronizer switch_1_sync (
-    .i_clk(i_Clk),
-    .i_input(i_Switch_1),
-    .o_input_sync(w_Switch_1)
-  );
 
-  wire w_Switch_2;
-  synchronizer switch_2_sync (
-    .i_clk(i_Clk),
-    .i_input(i_Switch_2),
-    .o_input_sync(w_Switch_2)
-  );
-
-  wire w_Switch_3;
-  synchronizer switch_3_sync (
-    .i_clk(i_Clk),
-    .i_input(i_Switch_3),
-    .o_input_sync(w_Switch_3)
-  );
-
-  // wire [7:0]  w_top;
-  // wire        w_top_valid;
-  // wire [8:0]  w_compare;
-  // wire        w_compare_valid;
-  // pwm_sequencer pwm_sequencer(
-  //   .i_clk(i_Clk),
-  //   .o_top(w_top),
-  //   .o_top_valid(w_top_valid),
-  //   .o_compare(w_compare),
-  //   .o_compare_valid(w_compare_valid)
-  // );
-
-  wire w_tick_stb;
-  wire w_beat_stb;
-  timing_strobe_generator #(
-    .CLOCK_FREQ(25_000_000)
-  ) pulse_generator (
+  wire [3:0] w_swithes;
+  go_board_switches switches (
     .i_clk(i_clk),
-    .o_tick_stb(w_tick_stb),
-    .o_beat_stb(w_beat_stb)
+    .i_switches({i_Switch_4, i_Switch_3, i_Switch_2, i_Switch_1}),
+    .o_switches(w_swithes)
   );
-  
-  wire [8:0] w_compare_pulse_1;
-  wire w_frame_pulse;
-  channel_1_pulse #(
+
+  wire [8:0] w_compare;
+  wire [3:0] w_frame_pulse;
+  audio_processing_unit #(
+    .CLOCK_FREQ(48_000),
     .NOTE_TABLE_FILE("resources/note_table_25MHz.txt"),
     .VIBRATO_TABLE_FILE("resources/vibrato_table_25MHz.txt")
-  ) pulse_1 (
-    .i_clk(i_Clk),
-    .i_tick_stb(w_tick_stb),
-    .i_note_stb(w_beat_stb),
-    .o_output(w_compare_pulse_1),
+  ) apu (
+    .i_clk(i_clk),
+    .i_mixer(w_swithes),
+    .o_sample(w_compare),
     .o_frame_pulse(w_frame_pulse)
   );
 
-  wire [8:0] w_compare_pulse_2;
-  channel_2_pulse #(
-    .NOTE_TABLE_FILE("resources/note_table_25MHz.txt")
-  ) pulse_2 (
-    .i_clk(i_Clk),
-    .i_tick_stb(w_tick_stb),
-    .i_note_stb(w_beat_stb),
-    .o_output(w_compare_pulse_2),
-    .o_frame_pulse()
-  );
-
-  wire [8:0] w_triangle_3_output;
-  channel_3_triangle #(
-    .NOTE_TABLE_FILE("resources/note_table_25MHz.txt")
-  ) triangle_3 (
-    .i_clk(i_Clk),
-    .i_tick_stb(w_tick_stb),
-    .i_note_stb(w_beat_stb),
-    .o_output(w_triangle_3_output),
-    .o_frame_pulse()
-  );
-
-  // Mixer
-  wire [8:0] w_compare =
-    (w_Switch_1? 0 : w_compare_pulse_1 )
-    + (w_Switch_2? 0 : w_compare_pulse_2)
-    + (w_Switch_3? 0 : w_triangle_3_output)
-    ;
   wire w_pwm;
   wire w_cycle_end;
   pwm pwm(
@@ -111,8 +47,7 @@ module pwm_top (
   );
 
   assign io_PMOD_1 = w_pwm;
-  assign io_PMOD_2 = w_frame_pulse;
+  assign io_PMOD_2 = w_frame_pulse[0];
   assign io_PMOD_3 = ~w_pwm;
-  // assign o_LED_1 = w_pwm;
 
 endmodule
