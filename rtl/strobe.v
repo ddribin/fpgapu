894db8d833098c7cdeeaabe45ad8e5025a911cea
Dave Dribin <dave-vcs@dribin.org>
2021-10-02 15:34:40 -0500

    checkpoint

--- rtl/strobe.v
+++ rtl/strobe.v
@@ -1,32 +1,46 @@
 `default_nettype none
 
 module strobe (
-  input wire  clk,
-  input wire  strobe_in
+  input wire        clk,
+  input wire        reset,
+  input wire        strobe_in,
+
+  output reg [3:0]  counter_1,
+  output reg [3:0]  counter_2
 );
   
   // Count clock ticks for simulation
   reg [7:0] clk_count;
   reg       strobe_in_q;
-  reg [3:0] counter_1;
-  reg [3:0] counter_2;
   
   always @(posedge clk) begin
-    clk_count <= clk_count + 1;
+    if (reset) begin
+      clk_count <= 0;
+    end else begin
+      clk_count <= clk_count + 1;
+    end
   end
 
   always @(posedge clk) begin
-    strobe_in_q <= strobe_in;
+    if (reset) begin
+      strobe_in_q <= 0;
+    end else begin
+      strobe_in_q <= strobe_in;
+    end
   end
 
   always @(posedge clk) begin
-    if (strobe_in) begin
+    if (reset) begin
+      counter_1 <= 0;
+    end else if (strobe_in) begin
       counter_1 <= counter_1 + 1;
     end
   end
 
   always @(posedge clk) begin
-    if (strobe_in_q) begin
+    if (reset) begin
+      counter_2 <= 0;
+    end else if (strobe_in_q) begin
       counter_2 <= counter_2 + 1;
     end
   end
--- rtl/strobe_test.cpp
+++ rtl/strobe_test.cpp
@@ -11,8 +11,10 @@ using UUT = Vstrobe_adapter;
 
 struct StrobeFixture : TestFixture<UUT>
 {
+    Input8 reset;
     Input8 strobe_in;
     StrobeFixture() :
+        reset(makeInput(&UUT::reset)), 
         strobe_in(makeInput(&UUT::strobe_in))
     {
     }
@@ -24,6 +26,7 @@ using Fixture = StrobeFixture;
 TEST_CASE_METHOD(Fixture, "Strobe Test", "[seq]")
 {
     bench.openTrace("/tmp/strobe.vcd");
+    reset.addInputs({{1, 1}, {2, 0}});
     strobe_in.addInputs({
         {5, 1},
         {6, 0},
